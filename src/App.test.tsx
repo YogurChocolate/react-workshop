b1666700a89ac5b701d2126eeb5ae02eb82b0a5f
SaiSea <saisea@saisea.com>
2020-03-22 15:21:25 +0800

    modify App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Haipeng Liu's first react App./i);
   expect(linkElement).toBeInTheDocument();
 });
